1dcb0c46de47002344cf6a2ff4e76814d481414d
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2021-12-27 09:40:24 +0900

    2021-12-27 10일차 수업 복습내용(복습 시작전 작성 후 전송실패 오류 해결)

--- kakao_spring_kim/src/main/java/com/spring/kakao/model/dto/NoticeDto.java
+++ kakao_spring_kim/src/main/java/com/spring/kakao/model/dto/NoticeDto.java
@@ -1,12 +1,14 @@
 package com.spring.kakao.model.dto;
 
 public class NoticeDto {
-
+	
+	// notice_mst
 	private int notice_code;
 	private String notice_title;
 	private String notice_writer;
 	private String notice_date;
 	private int notice_count;
+	// notice_dtl
 	private String notice_content;
 	private String originFileNames;
 	private String tempFileNames;
