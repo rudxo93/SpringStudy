342af4514171e45bd4fe3ba9e308adf12e8b4e12
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-01-04 23:48:31 +0900

    2022-01-04 11일차 수업 복습내용 1(spring boot 프로젝트 생성, @RestController에 대해)

---
+++ springboot_kakao_kim/src/main/java/com/springboot/kakao/SpringbootKakaoKimApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.kakao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKakaoKimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKakaoKimApplication.class, args);
+	}
+
+}
---
+++ springboot_kakao_kim/src/test/java/com/springboot/kakao/SpringbootKakaoKimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.kakao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKakaoKimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
